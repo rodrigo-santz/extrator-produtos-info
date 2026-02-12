dda704e0ac765f5dba23a183335a39708f95a737
rodrigo-santz <lemos.rod.contas@gmail.com>
2026-02-12 10:48:20 -0300

    atualizando url script

--- html/app.js
+++ html/app.js
@@ -1,5 +1,5 @@
 // ⚙️ CONFIGURAÇÃO: Altere a URL aqui se necessário
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxYBQXQmyP5G-96uZ4YE8F1pAI5qUJ83OYJ3yPQ6UtKPHJwNPIZt7OGpR997cZVAmrx/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyUWdjDHTM5OZYARZAKKL2gDQxcCZgxO4UknaWNMAxNqImxtFMwSCWgPiH174RO5bCD/exec';
 
 function postToAppsScript(scriptUrl, payload) {
     return new Promise((resolve, reject) => {
